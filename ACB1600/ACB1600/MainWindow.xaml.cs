78dfe52e597d50b5b095c199e15016996483b6c8
hawesome512 <hawesome@163.com>
2016-11-18 19:48:47 +0800

    界面设计

--- ACB1600/ACB1600/MainWindow.xaml.cs
+++ ACB1600/ACB1600/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace ACB1600
 {
@@ -20,9 +21,80 @@ namespace ACB1600
         /// </summary>
         public partial class MainWindow : Window
         {
+                List<RadioButton> rbList;
+                int rbIndex = 0;
                 public MainWindow()
                 {
                         InitializeComponent();
+                        initCtrls(12);
+                }
+
+                void initCtrls(int length)
+                {
+                        rbList = new List<RadioButton>();
+                        for (int i = 0; i < length; i++)
+                        {
+                                RadioButton rb = new RadioButton();
+                                rb.Name = "rb_" + i;
+                                rb.Checked += RadioButton_Checked;
+                                rb.Content = string.Format("{0:X}000", i);
+                                rb.Margin=new Thickness(75*i,0,0,0);
+                                grid_rbs.Children.Add(rb);
+                                rbList.Add(rb);
+                        }
+                        rbList[rbIndex].IsChecked = true;
+                        Binding binding1 = new Binding("Background");
+                        binding1.Source = rbList.First();
+                        rect_left.SetBinding(Rectangle.FillProperty, binding1);
+                        Binding binding2 = new Binding("Background");
+                        binding2.Source = rbList.Last();
+                        rect_right.SetBinding(Rectangle.FillProperty, binding2);
+                }
+
+                private void btn_close_Click(object sender, RoutedEventArgs e)
+                {
+                        this.Close();
+                }
+
+                private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+                {
+                        this.DragMove();
+                }
+
+                private void btn_min_Click(object sender, RoutedEventArgs e)
+                {
+                        this.WindowState = WindowState.Minimized;
+                }
+
+                private void btn_size_Click(object sender, RoutedEventArgs e)
+                {
+                        if (this.WindowState == WindowState.Normal)
+                        {
+                                this.WindowState = WindowState.Maximized;
+                        }
+                        else
+                        {
+                                this.WindowState = WindowState.Normal;
+                        }
+                }
+
+                private void RadioButton_Checked(object sender, RoutedEventArgs e)
+                {
+                        animateMove();
+                }
+
+                private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+                {
+                        rbIndex += (int)-e.Delta / 120;
+                        rbIndex = rbIndex < 0 ? 0 : (rbIndex == rbList.Count ? rbList.Count-1 : rbIndex);
+                        rbList[rbIndex].IsChecked = true;
+                        animateMove();
+                }
+
+                private void animateMove()
+                {
+                        ThicknessAnimation anim = new ThicknessAnimation(new Thickness(1, -rbIndex*200, 1, 1), new Duration(TimeSpan.FromSeconds(0.3)));
+                        grid_content.BeginAnimation(Grid.MarginProperty, anim);
                 }
         }
 }
